6539557ecc15a06109b45c40a85d6d242fc637b2
Jalpapatel0802 <jalpapatel0802@gmail.com>
2022-07-27 14:18:51 +0530

    adding new model

--- model.py
+++ model.py
@@ -1,1 +1,12 @@
 print("All code related to machine learninig model")
+
+
+def model1():
+    print("Adding code for model1")
+
+def model2():
+    print("Adding code for model2")
+
+
+def model3():
+    print("Adding code for model3")
